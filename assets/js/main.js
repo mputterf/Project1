72db801f8252d79e93d74d65d3307e834976f8a1
twistedx <smgowing@hotmail.com>
2019-03-13 16:07:37 -0700

    working on weather api

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,12 @@ var mainApp = {};
     var uid = null;
     var userName = "";
     var postal = "";
-    deBugger = false;
+    countryCode = "";
+    deBugger = true;
 
     firebase.auth().onAuthStateChanged(function (user) {
-       
-
         if (user) {
-            // User is signed in.
+            // Run once the User is signed in.
             if(deBugger){
                 console.log(user);
             };
@@ -20,6 +19,7 @@ var mainApp = {};
             displayName();
             getWeather();
         } else {
+            //no user signed in
             uid = null;
             window.location.replace("index.html");
         }
@@ -42,24 +42,27 @@ var mainApp = {};
         $(".mainContent").append(newDiv);
 
     }
+
+    function getWeather() {
+        var apiKey = 'APPID=a7f3e822eb731f30ddbb12e9307014cb';
+        var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?' + apiKey + '&zip=' + postal + ',' + countryCode;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+            
+        }).then(function (weatherResponse) {
+            console.log(weatherResponse);
+        });
+    }
+
     //capture the users IP address and utilize it to pull news and weather
 
     $.get("https://ipinfo.io", function (response) {
         postal = response.postal;
+        countryCode = response.country;
         if (deBugger) {
             console.log(response);
         };
     }, "jsonp")
 
-
-    function getWeather() {
-        queryURL = 'http://api.openweathermap.org/data/2.5/forecast?zip={' + postal + '};'
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (weatherResponse) {
-            console.log(weatherResponse);
-        }
-        )
-    };
 })()

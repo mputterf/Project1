bf48a763838ee50e9daa248b153489c9133b3fd7
mputterf <michael.putterflam@gmail.com>
2019-03-13 22:36:56 -0700

    basic weather forecast working

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,8 +33,8 @@ var mainApp = {};
         firebase.auth().signOut();
     }
     mainApp.logOut = logOut;
-    $("#signOut").on("click", mainApp.logOut); 
-    
+    $("#signOut").on("click", mainApp.logOut);
+
     function displayName(){
 
         var newDiv = $("<div>");
@@ -49,10 +49,42 @@ var mainApp = {};
         $.ajax({
             url: queryURL,
             method: "GET"
-            
+
         }).then(function (weatherResponse) {
-            console.log(weatherResponse);
+            console.log(weatherResponse.list);
+            displayWeather(weatherResponse);
         });
+    }
+
+    function displayWeather(weatherResponse) {
+      // weatherResponse.list gives an array, so stuff in results is an array and should be accessed with results[i]
+      var results = weatherResponse.list;
+
+      for (var i=0; i<weatherResponse.list.length; i++){
+        // Create new div for each forecast
+        var weatherDiv = $("<div>");
+
+        // give it a boostrap card for a boarder
+        weatherDiv.addClass("card");
+        // Give the data a class.
+        weatherDiv.addClass("weather-data");
+        // Give each forecast an id
+        weatherDiv.attr("id", "weather-forecast-" + i);
+        // Display the date and time the forecast is for.
+        weatherDiv.append("<li> Date & Time: " + results[i].dt_txt + "</li>");
+        // Display temp (needs to be converted from K to F)
+        weatherDiv.append("<ul> Temperature: " + parseInt((results[i].main.temp - 273.15) * (9/5) + 32) + " F </ul>");
+        // Display wind speed
+        weatherDiv.append("<ul> Wind Speed: " + results[i].wind.speed + " mph </ul>");
+        // Status of the sky
+        weatherDiv.append("<ul>" + results[i].weather[0].main + "</ul>");
+
+        // Send it all to the html
+        $(".weather").append(weatherDiv);
+      }
+
+
+
     }
 
     //capture the users IP address and utilize it to pull news and weather

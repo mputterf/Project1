5099e9f7d6befbdead1cc07e3e19f07326b3c8b4
bufu2o2 <bufu2o2@gmail.com>
2019-03-16 10:17:59 -0700

    created branch and committing because i want to pull

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,13 @@ var mainApp = {};
 
 (function () {
     var firebase = app_firebase;
+    const db = firebase.database();
+    const dbr = db.ref();
     var uid = null;
     var userName = "";
     var postal = "";
-    countryCode = "";
+    countryCode = "us";
+    let uidState = false;
     deBugger = true;
 
     firebase.auth().onAuthStateChanged(function (user) {
@@ -16,9 +19,18 @@ var mainApp = {};
             };
             uid = user.uid;
             userName = user.displayName;
-            displayName();
-            getWeather();
-            getNews();
+          
+            
+            zipChange();
+            keyFinder();
+            //delay necessary due to the time it takes for the other functions to run
+            setTimeout(() => {
+                displayName();
+                zreturn();   
+                getWeather();
+              getNews();
+            }, 1000);
+
         } else {
             //no user signed in
             uid = null;
@@ -61,6 +73,10 @@ var mainApp = {};
       // weatherResponse.list gives an array, so stuff in results is an array and should be accessed with results[i]
       var results = weatherResponse.list;
 
+      // Creating weather wrapper to overwrite HTML every time new zip is made
+      let weatherWrapper = $("<div/>");
+      weatherWrapper.addClass("weather-wrapper");
+
       for (var i=0; i<weatherResponse.list.length; i++){
         // Create new div for each forecast
         var weatherDiv = $("<div>");
@@ -79,10 +95,15 @@ var mainApp = {};
         weatherDiv.append("<ul> Wind Speed: " + results[i].wind.speed + " mph </ul>");
         // Status of the sky
         weatherDiv.append("<ul>" + results[i].weather[0].main + "</ul>");
+        // populate the weatherWrapper
+        weatherWrapper.append(weatherDiv);
 
-        // Send it all to the html
-        $(".weather").append(weatherDiv);
+        console.log("THIS IS THE COUNTRY CODE" + countryCode);
       }
+      
+      // overwrite old news when zip is updated
+      $(".cardWeather").html(weatherWrapper);
+      console.log("weather Updated");
     }
 
     function getNews(){
@@ -99,6 +120,10 @@ var mainApp = {};
     }
     
      function displayNews(newsResponse){
+         // Creating news wrapper to overwrite HTML every time new zip is made
+         let newsWrapper = $("<div/>");
+        newsWrapper.addClass("news-Wrapper");
+
         for(var i = 0; i < 5; i++){  
         if(deBugger){console.log("this news loop is running")};
         // The list item that will house everything
@@ -116,21 +141,138 @@ var mainApp = {};
         newsItem.text(newsResponse.articles[i].title)
         // append the link to the new list item
         newsDiv.append(newsItem);
-        // put it on the html
-        $(".news").append(newsDiv);
+        // populate the newsWrapper
+        newsWrapper.append(newsDiv);
     
         }
 
+    // overwrite old news when zip is updated
+        $(".news").html(newsWrapper);
+        console.log("news Updated");
     }
 
-    //capture the users IP address and utilize it to pull news and weather
+    // sets placeholder as your zip
+        let zreturn = () => {
+            console.log(postal);
+            $('.zip-input').attr("placeholder", "Current Zip: " + postal);
+        }
+        
 
+    // set postal as a temp variable
+    let zipChange = () => {
+        $(".zip-btn").on("click", function() {
+            let zi = $(".zip-input").val().trim();
+                postal = zi;
+                getWeather();
+                getNews();
+                console.log(postal);
+        });
+    }
+
+    //error function to display prompt
+    let error = (id) => {
+        let tempW = $("<div/>");
+        tempW.css({
+            "position": "absolute",
+            "top": "calc(50% - 100px)",
+            "left": "calc(50% - 300px)",
+            "width": "600px",
+            "height": "200px",
+        });
+        tempW.addClass("error");
+        let temp = $("<div/>");
+        temp.css({
+            "position" : "relative",
+            "display": "grid",
+            "justify-items": "center",
+            "align-items": "center",
+            "z-index": "100",
+            "width": "600px",
+            "height": "200px",
+            "background-color": "red",
+            "color": "black",
+            "font-family": "Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif",
+            "font-weight": "bolder",
+            "border": "5px inset black",
+            "border-radius": "10px",
+            "font-size": "30px",
+            "text-align": "center"
+        });
+        let tempP = $("<p/>");
+        let close = $("<div/>");
+        close.text("X");
+        close.css({
+            "position": "absolute",
+            "top": "0",
+            "right": "10px",
+            "font-family": "Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif",
+            "font-weight": "bolder",
+            "font-size": "50px",
+            "color": "black",
+            "width": "50px",
+            "height": "50px"
+        });
+        close.attr("id", "close");
+        tempP.text(id);
+        temp.append(tempP).append(close);
+        tempW.append(temp);
+        $("body").append(tempW).on("click", "#close", function () {
+            $(this).parent().parent().remove();
+        });
+    }
+
+
+    // push zipcode and UID to db
+    let pushDB = () => {
+        dbr.push({
+            uid,
+            postal,
+        });
+        
+    }
+    
+    // finds key to current UID
+    let keyFinder = () => {
+        dbr.orderByChild("uid").equalTo(uid).on("value", (snap) => {
+            snap.forEach((child) => {
+                let uidKey = child.key;
+                console.log("this is the UID Key: " + uidKey);
+                console.log("Zipcode pulled from Firebase DB");
+                uidState = true;
+                postal = child.val().postal;
+            });
+        });
+        setTimeout(() => {
+            dbKeyChecker();
+        }, 500);
+        
+    }
+
+    // checks if youre in the db already
+    let dbKeyChecker = () => {
+        if(!uidState){
+            pullPostal();
+            setTimeout(() => {
+                pushDB();    
+            }, 500);
+        }
+    }
+
+  
+    //capture the users IP address and utilize it to pull news and weather
+let pullPostal = () => {
+    console.log("Zipcode Pulled from IP via ipinfo.io");
     $.get("https://ipinfo.io", function (response) {
         postal = response.postal;
         countryCode = response.country;
         if (deBugger) {
             console.log(response);
         };
-    }, "jsonp")
+    }, "jsonp");
+}
 
 })()
+
+    
+
+    

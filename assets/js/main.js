54947cbfbaea927988c7dfdd6314f5d490b017c9
vucat2000 <cnikolai@earthlink.net>
2019-03-19 23:02:52 -0400

    ticketmaster minimum viable product

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,6 +148,197 @@ var mainApp = {};
         //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=34.1103407,-118.25850960000002&radius=500&types=cafe&key=AIzaSyD2fMFXXjaU_--ubFbg8T6rLWaju98eAeI
     }
 
+    $('#ticketmaster').click(function(){ loadSearch(); return false; });
+
+    // loads the search into the main content div
+    function loadSearch() {
+        $("#welcome").empty();
+        var userInstructions = $("<div>Enter a keyword to search on</div>");
+        $("#welcome").append(userInstructions);
+        var searchInput = $("<input type='text' id='ticketmasterkeywordsearchfield'>");
+        $("#welcome").append(searchInput);
+        var space = $("<br><br>");
+        $("#welcome").append(space);
+        var searchButton = $("<button id='ticketmastersearch' type='button'>Search</button>");
+        $("#welcome").append(searchButton);
+    }
+
+    $(document).on("click","#ticketmastersearch", function () { 
+        convertZiptoLatLong();
+    });
+
+    //Google geocoder api: AIzaSyBiI_6I3sbIYjPXsk5L0TtCO84DCdFQU0Y 
+    function convertZiptoLatLong() {
+        var lat = '';
+        var lng = '';
+        var address = postal;
+        geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            lat = results[0].geometry.location.lat();
+            lng = results[0].geometry.location.lng();
+            var latlng = {lat, lng};
+            if (deBugger) {
+                console.log("latlng: ", latlng);
+            }
+            // call ticketmaster here
+            getTicketmasterEvents(latlng);
+        } else {
+            console.log("Geocode was not successful for the following reason: " + status);
+        }
+        });
+    }
+
+    function googlePlaces(latlng) {
+        // googlePlaces
+        if (deBugger) {
+           console.log(latlng);
+        }
+        
+        let baseUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+        let apiKey = 'AIzaSyD2fMFXXjaU_--ubFbg8T6rLWaju98eAeI';
+        const keys = {
+            location:`${latlng.lat},${latlng.lng}`,
+            radius: 500,
+            types: 'cafe',
+            key: apiKey
+        };
+        $.ajax({
+            url: `${baseUrl}?location=${keys.location}&radius=${keys.radius}&types=${keys.types}&key=${keys.key}`,
+            method: "GET"
+
+        }).then(function (data) {
+            console.log(data);    
+        });
+        //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=34.1103407,-118.25850960000002&radius=500&types=cafe&key=AIzaSyD2fMFXXjaU_--ubFbg8T6rLWaju98eAeI
+    }
+
+    //uses link #2
+    function getTicketmasterEvents(latlng) {
+        if (deBugger) {
+            console.log(latlng);
+         }
+        //https://developer.ticketmaster.com/api-explorer/v2/
+        //SB: 41.7132821, -86.21076719999996
+        //LA: 4.1103407,-118.25850960000002
+        
+        //https://app.ticketmaster.com/discovery/v2/events?apikey=BHuf4uL2WnsQL8kxNUsmYVVLnoKKAAE9&latlong=41.7132821,-86.21076719999996&radius=115&unit=miles&page=1&sort=name,date,asc&countryCode=US
+
+        //var keyword = $("#ticketmasterkeywordsearchfield").val().trim().replace(/\s/g,'%20');
+        var keyword = $("#ticketmasterkeywordsearchfield").val();
+        if (deBugger) {
+            console.log("keyword", keyword);
+            //https://app.ticketmaster.com/discovery/v2/events?apikey=BHuf4uL2WnsQL8kxNUsmYVVLnoKKAAE9&keyword=basketball%20field&countryCode=US
+        }
+        //TODO: have user put key in search field and search for that - stuck on multi-keyword entries.
+        //TODO: display all results on a page - display more result items like picture, etc
+        //TODO: what if you have more than 20 results? &page= (look at page information at the top) - make a arrow container
+        //TODO: make it all fit in the height of the maincontent tag
+        let baseUrl = 'https://app.ticketmaster.com/discovery/v2/events';
+        let apiKey = 'BHuf4uL2WnsQL8kxNUsmYVVLnoKKAAE9';
+        const keys = {
+            latlong:`${latlng.lat},${latlng.lng}`,
+            radius: 115,
+            unit: "miles",
+            pages: 1,
+            key: apiKey,
+            keyword: keyword, 
+            sort: "name,date,asc",
+            countryCode: "US"
+        };
+        if (deBugger) {
+            //console.log(`${baseUrl}?apikey=${keys.key}&latlong=${keys.latlong}&radius=${keys.radius}&unit=${keys.unit}&pages=${keys.pages}&sort=${keys.sort}&keyword=${keys.keyword}&countryCode=${keys.countryCode}`);
+            console.log(`${baseUrl}?apikey=${keys.key}&keyword=${keys.keyword}`);
+        }
+        $.ajax({
+            //url: `${baseUrl}?apikey=${keys.key}&latlong=${keys.latlong}&radius=${keys.radius}&unit=${keys.unit}&pages=${keys.pages}&sort=${keys.sort}&keyword=${keys.keyword}&countryCode=${keys.countryCode}`,
+            url: `${baseUrl}?apikey=${keys.key}&keyword=${keys.keyword}`,
+            method: "GET"
+        }).then(function (data) {
+            if (deBugger) {
+                console.log(data);
+                console.log(data._embedded.events[0].name);
+            }
+            displyTicketmasterResults(data);
+        });
+    }
+
+    //TODO: see what page we are on.  calculate total number of pages.  if there are more events, display a right chevron.  if we are on page > 0, diesplay a left chevron.  
+    //TODO: when click on the event, then display details and have a back button as well.  
+    //when a left chevron is clicked, go to the previous page, when a right chevron is clicked, go to the next page.   
+    function displyTicketmasterResults(results) {
+        $("#welcome").empty();
+        //results._embedded.events gives an array, so stuff in results is an array and should be accessed with results[i]
+      var results = results._embedded.events;
+
+      // Creating weather wrapper to overwrite HTML every time new zip is made
+      let ticketmasterWrapper = $("<div>");
+      ticketmasterWrapper.addClass("ticketmaster-wrapper");
+      // Boostrap card
+      ticketmasterWrapper.addClass("card");
+      ticketmasterWrapper.addClass("text-success");
+
+      // Card header
+      let ticketmasterEventsTextDiv = $("<div/>");
+      ticketmasterEventsTextDiv.addClass("card-header");
+      ticketmasterEventsTextDiv.html("<strong>Ticketmaster Events Near You</strong>");
+      ticketmasterWrapper.append(ticketmasterEventsTextDiv);
+      //TODO: need a wrapper div like below
+      var rightChevron = $("<span class='right-chevron glyphicon glyphicon-chevron-right' data-page='0'></span>");
+      var leftChevron = $("<span class='left-chevron glyphicon glyphicon-chevron-left' data-page='0'></span>");
+
+      let resultsList = $("<ul>");
+      resultsList.addClass("list-group");
+      resultsList.addClass("list-group-flush");
+
+      //should the be the top 20 events
+      for (var i=0; i<results.length; i++){
+        // Create new div for each forecast
+        var listItemDiv = $("<li>");
+        listItemDiv.addClass("list-group-item");
+        listItemDiv.attr("id", "ticketmaster-event-" + i);
+        
+        // Display the name of the event
+        var nameDiv = $("<div>");
+        nameDiv.attr("id", "ticketmaster-name-div");
+        nameDiv.text(results[i].name);
+        listItemDiv.append(nameDiv);
+
+        var wrapperDiv = $("<div>");
+        wrapperDiv.attr("id", "ticketmaster-date-and-venue-wrapper-div");
+        listItemDiv.append(wrapperDiv);
+
+        // Display the date of the event
+        var dateDiv = $("<div>");
+        dateDiv.attr("id", "ticketmaster-date-div");
+        dateDiv.text(results[i].dates.start.localDate);
+        wrapperDiv.append(dateDiv);
+
+        // Display the venue of the event
+        var venueDiv = $("<div>");
+        venueDiv.attr("id", "ticektmaster-venue-div");
+        venueDiv.text(results[i]._embedded.venues[0].name);
+        wrapperDiv.append(venueDiv);
+        
+        
+        //listItemDiv.text(results[i].name);
+  
+        // populate the ticketmasterWrapper
+        ticketmasterWrapper.append(listItemDiv);
+      }
+
+      // overwrite main content div
+      $("#welcome").append(ticketmasterWrapper);
+      console.log("Ticket Event Updated");
+    }
+
+
+    function sleep(delay) {
+        var start = new Date().getTime();
+        while (new Date().getTime() < start + delay);
+    }
+
+
+
     function getWeather() {
         var weatherAPIKey = '9602d3b72d584a3fad8204559191503';
         // Max days to return is 10 days according to the docs
@@ -195,15 +386,16 @@ var mainApp = {};
         // Give each forecast an id
         weatherDiv.attr("id", "weather-forecast-" + i);
         // Display the date and time the forecast is for.
+        weatherDiv.append("<img class=weather-condition src=https:" + results[i].day.condition.icon + " >");
         weatherDiv.append("<p class=weather-info>" + moment(results[i].date).format('ddd') + "</p>");
         // Display high for the day
         weatherDiv.append("<p class=weather-info> High: " + parseInt(results[i].day.maxtemp_f) + "°F </p>");
         // Display low for the day
         weatherDiv.append("<p class=weather-info> Low: " + parseInt(results[i].day.mintemp_f) + "°F </p>");
         // Display wind speed
-        weatherDiv.append("<p class=weather-info> Wind Speed: " + parseInt(results[i].day.maxwind_mph) + "mph </p>");
-        // Weather condition
-        weatherDiv.append("<img class=weather-condition src=https:" + results[i].day.condition.icon + " >");
+        // weatherDiv.append("<p class=weather-info> Wind Speed: " + parseInt(results[i].day.maxwind_mph) + "mph </p>");
+        // // Weather condition
+  
         // populate the weatherWrapper
         weatherWrapper.append(weatherDiv);
       }
@@ -238,13 +430,13 @@ var mainApp = {};
         // Store the link
         var newsItem = $("<a>");
         // thumbnail
-        var newsImage = $("<img>");
+         var newsImage = $("<img>");
         // assign a news image class
-        newsImage.addClass("news-img");
+         newsImage.addClass("news-img");
         // set img source
-        newsImage.attr("src", newsResponse.articles[i].urlToImage);
+         newsImage.attr("src", newsResponse.articles[i].urlToImage);
         // clear fix for the thumbnails
-        newsDiv.addClass("clearfix");
+         newsDiv.addClass("clearfix");
         // Giving each news item the class and an id
         newsDiv.addClass("news-data");
         newsDiv.attr("id", "news-headlines-" + i);
@@ -294,7 +486,11 @@ var mainApp = {};
                 postal = zi;
                 getWeather();
                 getNews();
-                console.log(postal);
+                $("#welcome").remove();
+                displayName();
+                if (deBugger){
+                    console.log(postal);
+                }
         });
     }
 
